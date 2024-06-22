dba3f488ac5d91b063a18f5308b789992f1cfd12
bunita123 <158835321+bunita123@users.noreply.github.com>
2024-06-22 12:07:42 +0530

    Update assessment.sol

--- assessment.sol
+++ assessment.sol
@@ -1,24 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
 /*
 Contract successfully uses require()
 Contract successfully uses assert()
 Contract successfully uses revert() statements*/
 
-contract MyContract{
-    function testrequire(uint _i)public pure{
-        require(_i>=10,"Input must be less than 10");
-    }
+contract SchoolGrading{
+    enum Grade{A,B,F} //to define enum type"Grade"
+    
+    // Function to assign a grade to a student
 
-    uint public num;
-    function testAssert()public view{
-        assert(num==0);
-    }
-
-    function testRevert(uint _i)public pure{
-        if(_i<=10){
-            revert("input must be greater than 10");
+    function gradeRequire(uint score)public pure returns(Grade) {
+        require(score <=50,"Score cannot be greater than 50");
+            
+        Grade grade;
+        if(score>=40){
+            grade=Grade.A;
         }
+        else if(score>=30){
+            grade=Grade.B;
+        }
+        else {
+            grade=Grade.F;
+        }
+           return grade; // return the grade here after setting it
+    }
+
+   // Assert that a valid grade was assigned (should never fail)
+
+    function gradeAssert( uint score) public pure returns(Grade){
+            require(score <= 50,"Score cannot be greater than 50");
+             Grade grade;
+        if (score >= 40) {
+            grade = Grade.A;
+        } else if (score >= 30) {
+            grade = Grade.B;
+        } else {
+            grade = Grade.F;
+        }
+
+         // Assert the correctness of the grade assignment
+        assert((grade == Grade.A && score >= 40) || // A requires 40+
+               (grade == Grade.B && score >= 30 && score < 40) || // B requires 30-39
+               (grade == Grade.F && score < 30)); // F for less than 30
+
+        return grade;
+    }
+           
+    function gradeRevert(uint score)public pure  returns(Grade) {
+        if(score > 50) {
+            revert("Score cannot be greater than 50");
+        }
+
+        Grade grade;
+        if (score >= 40) {
+            grade = Grade.A;
+        } else if (score >= 30) {
+            grade = Grade.B;
+        } else {
+            grade = Grade.F;
+        }
+        return grade;
     }
 }
+
+
